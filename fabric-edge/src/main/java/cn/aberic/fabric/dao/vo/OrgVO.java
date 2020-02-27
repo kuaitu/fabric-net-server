94c6e70fb8d8be3fe287ddcd7033e9cf99aeee04
kuaitu <657781930@qq.com>
2020-02-27 14:30:07 +0800

    1、添加docker配置

--- fabric-edge/src/main/java/cn/aberic/fabric/dao/vo/OrgVO.java
+++ fabric-edge/src/main/java/cn/aberic/fabric/dao/vo/OrgVO.java
@@ -3,10 +3,12 @@ package cn.aberic.fabric.dao.vo;
 import cn.aberic.fabric.dao.entity.Org;
 import cn.aberic.fabric.dao.entity.Peer;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class OrgVO extends Org {
     private List<Peer> peerList;
 }
